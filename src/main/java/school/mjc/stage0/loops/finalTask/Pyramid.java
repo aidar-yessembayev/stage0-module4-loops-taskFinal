a81499f4fb466291b8daec8fb6034b069a243b6e
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2022-08-15 14:20:52 +0600

    Update Pyramid.java
    init

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = cathetusLength; j > i; j--) {
+                System.out.print(" ");
+            }
 
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
