4a2cdef180616d10db23449105cc23b6fc0d31aa
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2022-08-15 14:19:31 +0600

    Update HalfPyramid.java
    init

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = cathetusLength; j > 0; j--) {
+                if (j > i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(j);
+                }
+            }
 
+            for (int k = 2; k < cathetusLength; k++) {
+                if (k > i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(k);
+                }
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
